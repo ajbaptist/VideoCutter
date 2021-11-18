5839992dc6061d8f764d4dca820564aeed32cfd0
baptistjo01 <ajbaptist18@gmail.com>
2021-11-18 15:25:15 +0530

    first

---
+++ lib/video_cutter.dart
@@ -0,0 +1,9 @@
+library video_cutter;
+
+export 'package:video_cutter/src/video_viewer.dart';
+export 'package:video_cutter/src/trimmer.dart';
+export 'package:video_cutter/src/trim_editor.dart';
+export 'package:video_cutter/src/trim_editor_painter.dart';
+export 'package:video_cutter/src/thumbnail_viewer.dart';
+export 'package:video_cutter/src/file_formats.dart';
+export 'package:video_player/video_player.dart';
